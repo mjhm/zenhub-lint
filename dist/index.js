6fe9a25aae90a7c8b353442daeded2614646829b
John McLaughlin <john.mclaughlin@originate.com>
2021-07-04 07:33:48 -0700

    test7

--- dist/index.js
+++ dist/index.js
@@ -9339,6 +9339,7 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(6024);
 const github = __nccwpck_require__(5016);
+const https = __nccwpck_require__(7211);
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -9348,7 +9349,17 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`3. The event payload: ${payload}`);
+  console.log('3 sending get request')
+  https.get("https://example.com", (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+    res.on('data', (d) => {
+      console.log('data', data)
+    });
+  })
+  console.log('3 sent get request')
+  
 } catch (error) {
   core.setFailed(error.message);
 }
