c795b3dab16fcfd48bdc29f3c488afa27dbee3c2
John McLaughlin <john.mclaughlin@originate.com>
2021-07-04 07:07:56 -0700

    test5

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`3. The event payload: ${payload}`);
+  console.log('3 sending get request')
   https.get("https://example.com", (res) => {
     console.log('statusCode:', res.statusCode);
     console.log('headers:', res.headers);
@@ -18,7 +19,7 @@ try {
       console.log('data', data)
     });
   })
-  console.log('2 sent get request')
+  console.log('3 sent get request')
   
 } catch (error) {
   core.setFailed(error.message);
